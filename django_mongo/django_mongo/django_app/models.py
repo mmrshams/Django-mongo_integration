20af5f00d1f64000cb460927ce6bdf9574d7b75b
mmrshams <mmrshams00@gmail.com>
2019-07-08 01:04:49 +0430

    change resources to store dynamic json  -  create new json sample for request - some changes in requirment file

--- django_mongo/django_mongo/django_app/models.py
+++ django_mongo/django_mongo/django_app/models.py
@@ -1,12 +1,19 @@
 from __future__ import unicode_literals
-from mongoengine import Document, fields
+from mongoengine import fields,DynamicDocument,EmbeddedDocument,DynamicEmbeddedDocument
 
 
-class Tool(Document):
+class ToolInput(DynamicEmbeddedDocument):
+    name = fields.StringField()
+    value = fields.DynamicField()
+
+
+class Tool(DynamicDocument):
     sender = fields.StringField(required=True)
     description = fields.StringField(required=True)
     priority = fields.IntField(required=True)
     datetime = fields.DateTimeField(required=True)
+    inputs = fields.ListField(fields.EmbeddedDocumentField(ToolInput))
+    fields.DynamicField()
 
 
 
--- django_mongo/django_mongo/urls.py
+++ django_mongo/django_mongo/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
-from rest_framework import routers
-from  .django_app import views
+from rest_framework_mongoengine import routers
+from .django_app import views
 
 router = routers.DefaultRouter()
 router.register(r'api', views.ToolViewSet, base_name='api')
@@ -9,6 +9,5 @@ router.register(r'api', views.ToolViewSet, base_name='api')
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('', include(router.urls))
 ]
